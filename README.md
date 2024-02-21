4e795dbb71c874e8b75bb389b5627465d515570c
ysqluck <2183922902@qq.com>
2024-02-21 16:35:41 +0800

    新增了动态绘制线&面的代码

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## cesium常用方法封装
 ### 方法 initMouse：该方法修改了cesium的控制方式将左键设置为拖动相机，滚轮控制相机的前进后退，邮件控制相机的视角
-### 方法 getModelHeight 根据经纬度获取高程 （如果坐标点位置无3dTileset则有可能获取失败，获取失败返回结果为[lng,lat,-1]）
+### 方法 getModelHeight 根据经纬度获取高程 （如果坐标点位置无3dTiles则有可能获取失败，获取失败返回结果为[lng,lat,-1]）
 ### 方法 getDistance 获取两组经纬度的距离 单位：米
 ### 方法 windowPositionToWGS84 将屏幕坐标转换为WGS84坐标
 ### 方法 Cartesian3ToWGS84 将笛卡尔3坐标转换为WGS84坐标
--- index.js
+++ index.js
@@ -62,6 +62,7 @@ function getDistance(lng1, lat1, lng2, lat2) {
     s = s * EARTH_RADIUS;// EARTH_RADIUS;
     s = Math.round(s * 10000) / 10000; //输出为米
     s = s.toFixed(0);
+    console.log(s)
     return s;
 }
 
@@ -110,21 +111,76 @@ function dashLine(color) {
     })
 }
 // 绘制图元
+function getColor(option) {
+    if (option && option.color &&
+        typeof option.color.r === 'number' &&
+        typeof option.color.g === 'number' &&
+        typeof option.color.b === 'number' &&
+        typeof option.color.a === 'number') {
+        return Cesium.Color.fromBytes(option.color.r, option.color.g, option.color.b, option.color.a);
+    }
+    else {
+        return Cesium.Color.BLACK;
+    }
+}
+// 点标签的动态展示
+let showWhichPointId = ""
+const changeShowWhichPointLabel = (newId) => {
+    showWhichPointId = newId
+}
 /*
 @params
 entityPositionArr：c3ArrLIst
 */
 function resolveShape(type, option, entityPositionArr) {
+    if (typeof type !== 'string') {
+        throw new Error(`Invalid type: type should be a string, but received ${typeof type}`);
+    }
+
+    if (Object.prototype.toString.call(option) !== '[object Object]') {
+        throw new Error(`Invalid option: option should be an object, but received ${typeof option}`);
+    }
+
+    if (!Array.isArray(entityPositionArr)) {
+        throw new Error('Invalid entityPositionArr: entityPositionArr should be an array');
+    }
+    const color = getColor(option);
     switch (type.toLowerCase()) {
+        case 'point':
+            return {
+                name: `point`,
+                position: entityPositionArr[0],
+                point: {
+                    color: color,
+                    pixelSize: option.pixelSize ?? 10,
+                    outlineColor: option && option.outLineColor ? Cesium.Color.fromBytes(option.outLineColor.r, option.outLineColor.g, option.outLineColor.b, option.outLineColor.a) : Cesium.Color.YELLOW,
+                    outlineWidth: option.outLineWidth ?? 5,
+                    ...option.pointOption,
+                },
+                label: {
+                    // 是否显示
+                    show: new Cesium.CallbackProperty(() => {
+                        if (option.isLabelShow) {
+                            return true
+                        } else if (showWhichPointId === option.entityID) {
+                            return true
+                        } else {
+                            return false
+                        }
+                    }),
+                    ...option.labelOption,
+                }
+            }
         case 'line':
             return {
                 name: "polyline",
                 entityType: "polyline",
                 polyline: {
                     positions: new Cesium.CallbackProperty(() => entityPositionArr, false),
-                    clampToGround: option.ClampToGround ?? true,
+                    clampToGround: option.ClampToGround ?? false,
                     width: option.width ?? 10,
-                    material: option.color ? Cesium.Color.fromBytes(color.r, color.g, color.b, color.a) : Cesium.Color.YELLOW,
+                    material: color,
+                    ...option.polylineOption
                 }
             };
         case 'poly':
@@ -133,13 +189,14 @@ function resolveShape(type, option, entityPositionArr) {
                 entityType: "polygon",
                 polygon: {
                     hierarchy: new Cesium.CallbackProperty(() => new Cesium.PolygonHierarchy(entityPositionArr), false),
-                    outline: option.outline ?? true,
-                    outlineColor: Cesium.Color.GRAY,
-                    perPositionHeight: option.perPositionHeight ?? true,
+                    outline: option.outline ?? false,
+                    outlineColor: option && option.outLineColor ? Cesium.Color.fromBytes(option.outLineColor.r, option.outLineColor.g, option.outLineColor.b, option.outLineColor.a) : Cesium.Color.YELLOW,
+                    perPositionHeight: option.perPositionHeight ?? false,
                     material: new Cesium.ColorMaterialProperty(
                         option.color ? Cesium.Color.fromBytes(option.color.r, option.color.g, option.color.b, option.color.a) :
                             Cesium.Color.WHITE
                     ),
+                    ...option.polyOption
                 }
             };
         case 'dashed':
@@ -150,7 +207,8 @@ function resolveShape(type, option, entityPositionArr) {
                     positions: new Cesium.CallbackProperty(() => entityPositionArr, false),
                     clampToGround: option.isClampToGround ?? true,
                     width: option.width ?? 10,
-                    material: dashLine(option.color)
+                    material: dashLine(option.color),
+                    ...option.dashedOption
                 }
             };
         case 'circle':
@@ -161,9 +219,8 @@ function resolveShape(type, option, entityPositionArr) {
                 ellipse: {
                     semiMinorAxis: option.r ?? 1,
                     semiMajorAxis: option.r ?? 1,
-                    material: option.color ?
-                        Cesium.Color.fromBytes(option.color.r, option.color.g, option.color.b, option.color.a) :
-                        Cesium.Color.WHITE,
+                    material: color,
+                    ...option.circleOption
                 }
             };
         case 'box':
@@ -176,7 +233,8 @@ function resolveShape(type, option, entityPositionArr) {
                         option.color ?
                             Cesium.Color.fromBytes(option.color.r, option.color.g, option.color.b, option.color.a) :
                             Cesium.Color.WHITE
-                    )
+                    ),
+                    ...option.boxOption
                 }
             };
         default:
@@ -185,7 +243,6 @@ function resolveShape(type, option, entityPositionArr) {
 }
 
 const drawShape = (viewer, type, entityPositionArr, option = {}) => {
-    option.entityID = option.entityID || option.ID;
     if (viewer.entities.getById(option.entityID)) {
         viewer.entities.removeById(option.entityID);
     }
@@ -203,3 +260,228 @@ const drawShape = (viewer, type, entityPositionArr, option = {}) => {
 
     return shape;
 }
+const convertPickToCartesian3 = (viewer, pickPoint) => {
+    let item = Cartesian3ToWGS84(pickPoint);
+    return WGS84ToCartesian3(viewer, item.lng, item.lat, item.alt);
+};
+function removeMouseAction(handler) {
+    handler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK);
+    handler.removeInputAction(Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+    handler.removeInputAction(Cesium.ScreenSpaceEventType.RIGHT_CLICK);
+    handler.removeInputAction(Cesium.ScreenSpaceEventType.MIDDLE_CLICK);
+    handler = null
+}
+// dynamicDrawShape
+let pointsCollectionList = []
+const dynamicDrawShape = (viewer, type, payload, fn) => {
+    viewer._container.style.cursor = "crosshair";
+    let handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+    let entityPositionArr = [];
+    let shape = drawShape(viewer, type, entityPositionArr, payload);
+
+    handler.setInputAction((event) => {
+        let pick = viewer.scene.pickPosition(event.position);
+        if (!Cesium.defined(pick)) {
+            const ray = viewer.camera.getPickRay(event.position);
+            pick = viewer.scene.globe.pick(ray, viewer.scene);
+        }
+        // if (Cesium.defined(pick)) {
+        if (!pointsCollectionList.length) {
+            entityPositionArr.push(pick)
+            pointsCollectionList.push(Cartesian3ToWGS84(pick))
+        } else {
+            let nearestDistance, idx
+            for (let i in pointsCollectionList) {
+                let distance = (getDistance(pointsCollectionList[i][0], pointsCollectionList[i][1],
+                    Cartesian3ToWGS84(pick)[0], Cartesian3ToWGS84(pick)[1]
+                ))
+                if (distance < 0.5) {
+                    if (!nearestDistance) {
+                        nearestDistance = distance
+                        idx = i
+                    } else {
+                        if (distance < nearestDistance) {
+                            nearestDistance = distance
+                            idx = i
+                        }
+                    }
+                }
+            }
+            if (!nearestDistance) {
+                entityPositionArr.push(pick)
+                pointsCollectionList.push(Cartesian3ToWGS84(pick))
+            } else {
+                entityPositionArr.pop()
+                pick = WGS84ToCartesian3(
+                    viewer,
+                    pointsCollectionList[idx]
+                )
+                console.log(pick)
+                entityPositionArr.push(pick)
+                entityPositionArr.push(pick)
+            }
+
+        }
+
+        // }
+    }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+    handler.setInputAction((event) => {
+        let pick = viewer.scene.pickPosition(event.endPosition);
+        if (!Cesium.defined(pick)) {
+            const ray = viewer.camera.getPickRay(event.endPosition);
+            pick = viewer.scene.globe.pick(ray, viewer.scene);
+        }
+
+        if (pick && entityPositionArr.length === 1) {
+            entityPositionArr.push(pick);
+        }
+        entityPositionArr[entityPositionArr.length - 1] = pick;
+    }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+
+    handler.setInputAction((event) => {
+        entityPositionArr.pop();
+        removeMouseAction(handler);
+        viewer._container.style.cursor = "default";
+        if (fn) {
+            let locations;
+            if (type === 'line') {
+                locations = [...shape.polyline.positions.getValue(new Date())];
+                shape.locations = locations.map(item => Cartesian3ToWGS84(item));
+            } else if (type === 'poly') {
+                const hierarchyValue = shape.polygon.hierarchy.getValue(new Date());
+                locations = hierarchyValue.positions ? [...hierarchyValue.positions] : [];
+                shape.locations = locations.map(item => Cartesian3ToWGS84(item));
+            }
+            fn(shape);
+        }
+    }, Cesium.ScreenSpaceEventType.RIGHT_CLICK);
+
+    return { shape, handler };
+};
+function drawWall(viewer, wallList, option) {
+    if (!option.entityID) {
+        option.entityID = option.ID || randomString(6)
+    }
+    if (option.entityID && viewer.entities.getById(option.entityID)) {
+        viewer.entities.removeById(option.entityID)
+    }
+    const img = new Image().scr = './dynamicWall.png';
+    wallList.push(wallList[0]);
+    let wallPosList = JSON.parse(JSON.stringify(wallList))
+    wallPosList.map(item => {
+        item[2] = option.wallHeight || 60
+        return item
+    })
+    //动态墙材质
+    function DynamicWallMaterialProperty(options) {
+        // 默认参数设置
+        this._definitionChanged = new Cesium.Event();
+        this._color = undefined;
+        this._colorSubscription = undefined;
+        this.color = options.color;
+        this.duration = options.duration;
+        this.trailImage = options.trailImage;
+        this._time = new Date().getTime();
+    }
+    Object.defineProperties(DynamicWallMaterialProperty.prototype, {
+        isConstant: {
+            get: function () {
+                return false;
+            },
+        },
+        definitionChanged: {
+            get: function () {
+                return this._definitionChanged;
+            },
+        },
+        color: Cesium.createPropertyDescriptor("color"),
+    });
+    DynamicWallMaterialProperty.prototype.getType = function (time) {
+        return "DynamicWall";
+    };
+    DynamicWallMaterialProperty.prototype.getValue = function (
+        time,
+        result
+    ) {
+        if (!Cesium.defined(result)) {
+            result = {};
+        }
+        result.color = Cesium.Property.getValueOrClonedDefault(
+            this._color,
+            time,
+            Cesium.Color.WHITE,
+            result.color
+        );
+        if (this.trailImage) {
+            result.image = this.trailImage;
+        } else {
+            result.image = Cesium.Material.DynamicWallImage;
+        }
+
+        if (this.duration) {
+            result.time =
+                ((new Date().getTime() - this._time) % this.duration) /
+                this.duration;
+        }
+        viewer.scene.requestRender();
+        return result;
+    };
+    DynamicWallMaterialProperty.prototype.equals = function (other) {
+        return (
+            this === other ||
+            (other instanceof DynamicWallMaterialProperty &&
+                Cesium.Property.equals(this._color, other._color))
+        );
+    };
+    Cesium.DynamicWallMaterialProperty = DynamicWallMaterialProperty;
+    Cesium.Material.DynamicWallType = "DynamicWall";
+    Cesium.Material.DynamicWallImage = img;
+    Cesium.Material.DynamicWallSource =
+        "czm_material czm_getMaterial(czm_materialInput materialInput)\n\
+                                                {\n\
+                                                czm_material material = czm_getDefaultMaterial(materialInput);\n\
+                                                vec2 st = materialInput.st;\n\
+                                                vec4 colorImage = texture2D(image, vec2(fract(st.t - time), st.t));\n\
+                                                vec4 fragColor;\n\
+                                                fragColor.rgb = color.rgb / 1.0;\n\
+                                                fragColor = czm_gammaCorrect(fragColor);\n\
+                                                material.alpha = colorImage.a * color.a;\n\
+                                                material.diffuse = color.rgb;\n\
+                                                material.emission = fragColor.rgb;\n\
+                                                return material;\n\
+                                                }";
+    Cesium.Material._materialCache.addMaterial(
+        Cesium.Material.DynamicWallType,
+        {
+            fabric: {
+                type: Cesium.Material.DynamicWallType,
+                uniforms: {
+                    color: new Cesium.Color(1.0, 1.0, 1.0, 1),
+                    image: Cesium.Material.DynamicWallImage,
+                    time: 0,
+                },
+                source: Cesium.Material.DynamicWallSource,
+            },
+            translucent: function (material) {
+                return true;
+            },
+        }
+    );
+    // 绘制墙体
+    let wall = viewer.entities.add({
+        id: option.entityID,
+        wall: {
+            positions: Cesium.Cartesian3.fromDegreesArrayHeights(
+                wallPosList.flat()
+            ),
+            // 设置高度
+            material: new Cesium.DynamicWallMaterialProperty({
+                color: Cesium.Color.fromBytes(10, 186, 181).withAlpha(0.35),
+                duration: 1900,
+            }),
+        },
+    });
+    return wall
+}
+
